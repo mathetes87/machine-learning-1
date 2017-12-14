1cd88460fd67ab29f6321b176c4ee4837948a6f5
Juan Roberto Honorato <juanrhonorato@gmail.com>
2017-12-13 23:39:54 -0300

    Start training

--- projects/capstone/Forex Predict/feed_data.py
+++ projects/capstone/Forex Predict/feed_data.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import os, math
+import os, sys, math
 import pandas as pd
+import numpy as np
 
 class data_feed(object):
 	def __init__(self, *args, **kwargs):
 		# required parameters
 		# filepath, window, percentage, dataset
-		self.all_data = pd.read_csv(kwargs['filepath'])
+		self.filepath = os.path.join(sys.path[0], kwargs['filepath'])
+		self.all_data = pd.read_csv(self.filepath).drop(['time'], axis=1)
 		if kwargs['dataset'] == 'train':
 			self.data = self.all_data[:int(len(self.all_data)*kwargs['percentage'])]
 		if kwargs['dataset'] == 'test':
@@ -21,14 +23,14 @@ class data_feed(object):
 		# check that next batch doesn't go beyond data size
 		if self.index_data + self.window_size < self.length:
 			end = start + self.window_size
-			to_return = self.data[start:end]
+			to_return = self.data[start:end+1]
 		else:
 			# next batch goes beyond data size
 			# first, extract remaining data
 			first_piece = self.data[start:]
 			# now second piece
 			end = self.window_size - len(first_piece)
-			second_piece = self.data[:end]
+			second_piece = self.data[:end+1]
 			# concat both pieces
 			to_return = pd.concat([first_piece, second_piece])
 		# finally check if we reached the end of the dataset
@@ -36,16 +38,43 @@ class data_feed(object):
 		if self.index_data == self.length:
 			self.index_data = 0
 
-		to_return = to_return.as_matrix() 
-		return 
+		to_return = to_return.as_matrix()
 
-	def feed(self):
+		X = to_return[:-1]
+		# if close price is less than 0.03% of previous close price, we will label it as a shy movement
+		# labels: way_up, shy_up, shy_down, way_down as a onehot vector
+		# this threshold was selected by trial and error. It gives, for the first 50,000 candles, 
+		# a distribution of [6402, 18188, 19202, 6208] which seems about right
+		last_candle = X[-1][1]
+		next_candle = to_return[-1][1]
+		relation = float(next_candle - last_candle)/last_candle
+		percentage = 0.03/100
+		if relation > percentage:
+			move = 3
+		elif relation >= 0:
+			move = 2
+		elif relation > -percentage:
+			move = 1
+		else:
+			move = 0
+		y = np.zeros(4)
+		y[move] = 1
+
+		X = np.asarray(X, dtype=np.float32)
+		X = np.expand_dims(X, axis=0)
+		y = np.asarray(y, dtype=np.float32)
+		y = np.expand_dims(y, axis=0)
+
+		return (X, y)
+
+	def generate(self):
 		# loop through the data in order indefinitely
 		while True:
 			yield self.next_batch()
 
 if __name__ == '__main__':
-	feed = data_feed(filepath='data/EURUSD_M5_438K_Preprocessed.csv', window=32, percentage=0.9, dataset='train')
+	feed = data_feed(filepath='data/EURUSD_M5_438K_Preprocessed.csv', window=2, percentage=0.9, dataset='train')
+	print feed.next_batch()
 	#feed2 = data_feed(filepath='data/EURUSD_M5_438K_Preprocessed.csv', window=32, percentage=0.1, dataset='test')
 
  
--- projects/capstone/Forex Predict/train_network.py
+++ projects/capstone/Forex Predict/train_network.py
@@ -2,7 +2,32 @@
 # -*- coding: utf-8 -*-
 from feed_data import data_feed
 from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import LSTM
+from keras.layers import Dense, LSTM, Flatten, Dropout
+from keras.callbacks import TensorBoard
 
-train_data = data_feed(filepath='data/EURUSD_M5_438K_Preprocessed.csv', window=32, percentage=0.9, dataset='train')
+window = 64
+train_data = data_feed(filepath='data/EURUSD_M5_438K_Preprocessed.csv', window=window, percentage=0.9, dataset='train')
+test_data = data_feed(filepath='data/EURUSD_M5_438K_Preprocessed.csv', window=window, percentage=0.1, dataset='test')
+
+# design network
+model = Sequential()
+model.add(LSTM(64, input_shape=(window, 9)))
+model.add(Dropout(0.3))
+
+model.add(Dense(4, activation='softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='adam')
+
+# callbacks
+tensorboard = TensorBoard(log_dir='/tmp/tensorboard', write_graph=True)
+
+# fit network
+history = model.fit_generator(
+    generator=train_data.generate(),
+    steps_per_epoch=1000,
+    epochs=200,
+    verbose=1,
+    callbacks=[tensorboard],
+    validation_data=test_data.generate(),
+    validation_steps=100,
+    shuffle=False
+)
